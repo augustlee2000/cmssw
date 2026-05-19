53aa211645a1a49eceb932c2290c06bba0615b85
August J. Lee <77298066+augustlee2000@users.noreply.github.com>
2026-05-19 15:39:38 -0400

    Fix typo in loading ScoutingHadronicTopMonitor
    Add ScoutingHadronicTop Monitor

--- DQM/Integration/python/clients/ngt_dqm_sourceclient-live_cfg.py
+++ DQM/Integration/python/clients/ngt_dqm_sourceclient-live_cfg.py
@@ -63,6 +63,9 @@ process.scoutingCollectionMonitor.rho = ["hltScoutingPFPacker", "rho"]
 process.load("DQM.HLTEvF.ScoutingDileptonMonitor_cfi")
 process.ScoutingDileptonMonitorOnline.OutputInternalPath = "NGT/ScoutingOnline/DiLepton"
 
+process.laod("DQM.HLTEvF.ScoutingHadronicTopMonitor_cfi")
+process.ScoutingHadronicTopMonitorOnline.OutputInternalPath = "NGT/ScoutingOnline/HadronicTop"
+
 process.load("DQM.HLTEvF.ScoutingPi0Monitor_cfi")
 process.ScoutingPi0MonitorOnline.OutputInternalPath = "NGT/ScoutingOnline/PiZero"
 process.ScoutingPi0MonitorOnline.maxEta = 2.5
